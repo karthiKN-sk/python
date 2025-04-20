df8d6b73252ac28f91f2a13030182a73fae7003d
Karthikeyan.SK <karthikeyan.sk@grootan.com>
2025-04-20 18:15:23 +0530

    Initial commit

---
+++ Day0.py
@@ -0,0 +1,14 @@
+# Key Feature
+
+# 1. Easy to learn & read 
+#    hello world
+
+# 2. Multi-Purpose
+
+# 3. Interpreted Language
+
+# 4. Rich Standard Library
+
+# 5. Platform Independant
+
+# 6. Large Community & Ecosystem
